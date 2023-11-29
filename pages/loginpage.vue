70dcbcd49cee974142eda8dbd64d2d85a067e5f1
rastridge <rfa@me.com>
2023-11-29 10:55:09 -0500

    Centered login form

--- pages/loginpage.vue
+++ pages/loginpage.vue
@@ -3,60 +3,61 @@
 		<div v-if="alert.message" :class="`alert ${alert.type}`">
 			{{ alert.message }}
 		</div>
-		<div
-			v-if="!auth.isLoggedIn"
-			class="surface-card p-4 shadow-2 border-round w-full lg:w-7"
-		>
-			<div class="text-center mb-5">
-				<div class="text-900 md:text-2xl font-medium mb-3">
-					Login Admin User
-				</div>
-			</div>
-			<FormKit type="form" submit-label="Login" @submit="handleSubmit">
-				<FormKit
-					type="text"
-					name="username"
-					label="Username"
-					validation="required|length:3"
-					validation-visibility="live"
-					:validation-messages="{
-						matches: 'Username must be at least 3 chars',
-					}"
-				>
-				</FormKit>
-
-				<FormKit
-					type="password"
-					name="password"
-					label="Password"
-					validation="required|length:5"
-					validation-visibility="live"
-					:validation-messages="{
-						matches: 'Password must be at least 5 chars',
-					}"
-				>
-				</FormKit>
-
-				<div class="flex align-items-center justify-content-between mb-6">
-					<div class="flex align-items-center">
-						<Checkbox
-							id="rememberme1"
-							:binary="true"
-							v-model="keeploggedin"
-							class="mr-2"
-						></Checkbox>
-						<label for="rememberme1">Keep me logged in</label>
+		<div class="topsectioncenter">
+			<div
+				v-if="!auth.isLoggedIn"
+				class="topsectionitem surface-card p-4 shadow-2 border-round w-full lg:w-5"
+			>
+				<div class="text-center mb-5">
+					<div class="text-900 md:text-2xl font-medium mb-3">
+						Login Admin User
 					</div>
-
-					<nuxt-link
-						to="/resetpassword"
-						class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer"
-						active-class="active"
-						><a>Forgot password?</a></nuxt-link
-					>
 				</div>
-			</FormKit>
-			<Button label="Cancel" class="w-50" @click="cancelForm()"></Button>
+				<FormKit type="form" submit-label="Login" @submit="handleSubmit">
+					<FormKit
+						type="text"
+						name="username"
+						label="Username"
+						validation="required|length:3"
+						validation-visibility="live"
+						:validation-messages="{
+							matches: 'Username must be at least 3 chars',
+						}"
+					>
+					</FormKit>
+
+					<FormKit
+						type="password"
+						name="password"
+						label="Password"
+						validation="required|length:5"
+						validation-visibility="live"
+						:validation-messages="{
+							matches: 'Password must be at least 5 chars',
+						}"
+					>
+					</FormKit>
+
+					<div class="flex align-items-center justify-content-between mb-6">
+						<div class="flex align-items-center">
+							Keep me logged in&nbsp;
+							<Checkbox
+								:binary="true"
+								v-model="keeploggedin"
+								class="mr-2"
+							></Checkbox>
+						</div>
+
+						<nuxt-link
+							to="/resetpassword"
+							class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer"
+							active-class="active"
+							><a>Forgot password?</a></nuxt-link
+						>
+					</div>
+				</FormKit>
+				<Button label="Cancel" @click="cancelForm()"></Button>
+			</div>
 		</div>
 	</div>
 </template>
