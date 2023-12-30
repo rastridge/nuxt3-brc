0197c73076fc2722faee74be0b6b98df9daeb335
rastridge <rfa@me.com>
2023-12-29 19:30:29 -0500

    fit menu to phone screen

--- components/app/AppMenu.vue
+++ components/app/AppMenu.vue
@@ -11,15 +11,17 @@
 	.p-menuitem-text {
 		font-size: small;
 	}
-	/* clumsy attempt to bring menu items closer together */
-	@media (max-width: 960px) {
-		.p-menuitem-content {
-			margin-top: -5px;
-		}
-	}
 	@media (min-width: 992px) {
 		.p-menuitem-text {
 			font-size: large;
 		}
 	}
+
+	/* clumsy attempt to bring menu items closer together */
+	@media (max-width: 960px) {
+		.p-menuitem-content {
+			margin-top: -20px;
+		}
+	}
+	/* 		*/
 </style>
