74637a3b8cf428fa35a859d207cd508c455d7c18
rastridge <rfa@me.com>
2024-02-02 09:01:41 -0500

    cleaned up default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,10 @@
 <script setup>
-	import { usePrimeVue } from 'primevue/config'
-	// import AppTopBar from '../components/app/AppTopbar.vue'
-	// import AppFooter from '../components/app/AppFooter.vue'
 	import { useAuthStore } from '~/stores/authStore'
 	const auth = useAuthStore()
-
-	import { useMenuStore } from '@/stores'
-	const menuStore = useMenuStore()
-	// const customMenuItems = menuStore.getCustomMenuItems
 </script>
 
 <template>
 	<div>
-		<!-- default start up blue primary  theme? -->
-		<!-- <Link
-			rel="stylesheet"
-			:href="'https://cdn.jsdelivr.net/npm/primevue@3.15.0/resources/themes/vela-blue/theme.css'"
-		/> -->
-		<!-- 		<Link
-			rel="stylesheet"
-			:href="'https://cdn.jsdelivr.net/npm/primevue@3.15.0/resources/themes/saga-blue/theme.css'"
-		/> -->
-
 		<div
 			class="bg-transparent border-round-lg md:border-round-xl md:m-2 p-1 md:p-2"
 		>
