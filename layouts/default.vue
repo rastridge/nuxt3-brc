85800a33a0bb18a491f1b5ea9d0246364c347222
rastridge <rfa@me.com>
2024-02-11 11:36:22 -0500

    remve div

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,17 +4,15 @@
 </script>
 
 <template>
-	<div>
-		<div
-			class="bg-transparent border-round-lg md:border-round-xl md:m-2 p-1 md:p-2"
-		>
-			<app-header />
-		</div>
-		<div
-			class="surface-400 border-round-lg md:border-round-xl shadow-6 m-0 md:m-2 p-1 md:p-2"
-		>
-			<slot />
-		</div>
-		<div v-if="!auth.isLoggedIn"><AppFooter /></div>
+	<div
+		class="bg-transparent border-round-lg md:border-round-xl md:m-2 p-1 md:p-2"
+	>
+		<app-header />
 	</div>
+	<div
+		class="surface-400 border-round-lg md:border-round-xl shadow-6 m-0 md:m-2 p-1 md:p-2"
+	>
+		<slot />
+	</div>
+	<div v-if="!auth.isLoggedIn"><AppFooter /></div>
 </template>
