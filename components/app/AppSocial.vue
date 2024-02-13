3146a6cdd1f58d35206caac0f7bdb8a88cd6ffed
rastridge <rfa@me.com>
2024-02-13 15:37:38 -0500

    cnvert to primevue buttons

--- components/app/AppSocial.vue
+++ components/app/AppSocial.vue
@@ -1,13 +1,10 @@
 <template>
-	<div
-		class="border- border-white border-round-sm md:border-round-lg w-full md:w-12 lg:w-10 mx-auto"
-	>
-		<ul
-			class="flex gap-2 border- border-white w-30rem md:w-40rem lg:w-10 xl:w-8 mx-auto"
-		>
+	<div class="border- border-white border-round-sm md:border-round-lg">
+		<ul class="flex gap-1 border- border-white w-40rem lg:w-10 xl:w-8 mx-auto">
 			<li>
 				<Button
-					class="p-link my-text-style text-white hover:text-blue-200"
+					link
+					class="my-text-style text-white hover:text-blue-200"
 					@click="redirectToFacebook"
 				>
 					<i class="pi pi-facebook my-text-style" />
@@ -17,7 +14,8 @@
 
 			<li>
 				<Button
-					class="p-link my-text-style text-white hover:text-blue-200"
+					link
+					class="my-text-style text-white hover:text-blue-200"
 					@click="redirectToYoutube"
 				>
 					<i class="pi pi-twitter my-text-style" />
@@ -26,16 +24,18 @@
 			</li>
 			<li>
 				<Button
-					class="p-link my-text-style text-white hover:text-blue-200"
+					link
+					class="my-text-style text-white hover:text-blue-200"
 					@click="redirectToInstagram"
 				>
-					<i class="pi pi-instagram my-text-style" />
+					<i class="pi pi-twitter my-text-style" />
 					<span>instagram</span>
 				</Button>
 			</li>
 			<li>
 				<Button
-					class="p-link my-text-style text-white hover:text-blue-200"
+					link
+					class="my-text-style text-white hover:text-blue-200"
 					@click="redirectToRegister"
 				>
 					<i class="pi pi-user-plus my-text-style" />
@@ -44,7 +44,8 @@
 			</li>
 			<li>
 				<Button
-					class="p-link my-text-style text-white hover:text-blue-200"
+					link
+					class="my-text-style text-white hover:text-blue-200"
 					@click="redirectToFlagRegister"
 				>
 					<i class="pi pi-user-plus my-text-style" />
@@ -55,23 +56,23 @@
 	</div>
 </template>
 
-<script setup lang="ts">
-	function redirectToFacebook(event: any) {
+<script setup>
+	function redirectToFacebook(event) {
 		window.open('https://www.facebook.com/BuffaloRugbyClub/', '_blank')
 	}
-	function redirectToTwitter(event: any) {
+	function redirectToTwitter(event) {
 		window.open('https://twitter.com/BuffaloRFC', '_blank')
 	}
-	function redirectToYoutube(event: any) {
+	function redirectToYoutube(event) {
 		window.open('https://youtube.com/buffalorugby', '_blank')
 	}
-	function redirectToInstagram(event: any) {
+	function redirectToInstagram(event) {
 		window.open('https://instagram.com/buffalorugbyclub', '_blank')
 	}
-	function redirectToRegister(event: any) {
+	function redirectToRegister(event) {
 		navigateTo('/register')
 	}
-	function redirectToFlagRegister(event: any) {
+	function redirectToFlagRegister(event) {
 		navigateTo('/registerflag')
 	}
 </script>
